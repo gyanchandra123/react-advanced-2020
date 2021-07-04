cb0fdf8031247ac40485e55e4211031c42888aef
gyanchandra moirangthem <gyanchandra moirangthem>
2021-07-04 14:02:06 +0530

    42. changing object property's value using usestate()

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setperson] = useState({
+    name: "peter",
+    age: 24,
+    message: "random mesage",
+  });
+
+  const changeUseMessage = (newMessage) => {
+    setperson({
+      ...person,
+      message: newMessage,
+    });
+  };
+
+  return (
+    <>
+      <h4>{person.name}</h4>
+      <h4>{person.age}</h4>
+      <h4>{person.message}</h4>
+      <button onClick={() => changeUseMessage("new message")}  className='btn'> 
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
