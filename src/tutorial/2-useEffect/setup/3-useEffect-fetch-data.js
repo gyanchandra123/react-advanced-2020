ff7beecdef50de6646b6d7d7eedcb40cf3c057a4
gyanchandra moirangthem <gyanchandra moirangthem>
2021-07-04 22:17:01 +0530

    51. use effect() fetching data

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const url = 'https://api.github.com/users';
+const url = "https://api.github.com/users";
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users, usersState] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((users) => {
+        console.log(users);
+        usersState(users.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <>
+      <ul className='users'>
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default UseEffectFetchData;
