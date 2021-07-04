f062152f426bb409e71cb420c6f42f36ffadc535
gyanchandra moirangthem <gyanchandra moirangthem>
2021-07-04 13:48:52 +0530

    41. use state- display array's object value + delete all array item + delete each item separately

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+
+  const deletePerson = (id) => {
+    const filteredPeople = people.filter((person) => person.id !== id);
+    setPeople(filteredPeople);
+  };
+
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h3>{name}</h3>
+            <button onClick={()=>deletePerson(id)}>
+              delete
+            </button>
+          </div>
+        );
+      })}
+
+      <button type="button" className="btn" onClick={() => setPeople([])}>
+        clear all items
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
