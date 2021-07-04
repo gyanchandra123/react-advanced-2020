5196faf11f0fda03cd405633dc720846697ea90c
gyanchandra moirangthem <gyanchandra moirangthem>
2021-07-04 12:49:33 +0530

    39-use state -basics

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  //proofing the useState is a function returning an array of 2 values.
+  console.log(useState(1));
+  console.log(useState(1)[0]);
+  console.log(useState(1)[1]);
+
+  let initialTitle = "random title";
+
+  const [title, setTitle] = useState(initialTitle);
+
+  const changeTitle = () => {
+    if (title === "random title") {
+      setTitle("hello world");
+    } else {
+      setTitle("random title");
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <button type="button" onClick={changeTitle} className='btn'>
+        change title
+      </button>
+      <h3>{title}</h3>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
