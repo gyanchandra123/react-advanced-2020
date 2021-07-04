9a6b20885d2b9ae1a7494bec44922431b1075267
gyanchandra moirangthem <gyanchandra moirangthem>
2021-07-04 19:37:59 +0530

    47 useeffect basics

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [count, setCount] = useState(0);
+
+  // to check the working of the useffect:
+  useEffect(() => {
+    console.log("use effect");
+    document.title = `new doc title ${count}`;
+  });
+
+  console.log("component render");
+
+  return (
+    <>
+      <h2>useEffect Basics</h2>
+      <h3>count value :{count}</h3>
+      <button onClick={()=>setCount(count + 1)} className='btn'>increase count</button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
