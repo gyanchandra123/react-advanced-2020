613de6ce25b9d5b4760ac776e472bf8dd60f09f0
gyanchandra moirangthem <gyanchandra moirangthem>
2021-07-04 12:29:13 +0530

    38 before using the useState()

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = "book show";
+
+  return (
+    <div className="container">
+      <button type="button" onChange={()=>{title='new title'}}>
+        change the title name
+      </button>
+      <h2> {title}</h2>
+    </div>
+  );
 };
 
 export default ErrorExample;
+// without using the state feature in functional component, it's not 
+//possible to update any variable & render in UI.
