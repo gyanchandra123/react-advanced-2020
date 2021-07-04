5d923f423bd75f999dc611c8c90ea339defed17f
gyanchandra moirangthem <gyanchandra moirangthem>
2021-07-04 15:21:08 +0530

    44. multiple states replacing object state

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,25 +1,34 @@
 import React, { useState } from "react";
 
 const UseStateObject = () => {
+  /*
   const [person, setperson] = useState({
     name: "peter",
     age: 24,
     message: "random mesage",
-  });
+  }); */
+
+  const [name, setName] = useState("peter");
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState("random mesage");
 
   const changeUseMessage = (newMessage) => {
-    setperson({
+    //following the object way:
+    /*  setperson({
       ...person,
       message: newMessage,
-    });
+    }); */
+
+    //following multiple state way
+    setMessage(newMessage);
   };
 
   return (
     <>
-      <h4>{person.name}</h4>
-      <h4>{person.age}</h4>
-      <h4>{person.message}</h4>
-      <button onClick={() => changeUseMessage("new message")}  className='btn'> 
+      <h4>{name}</h4>
+      <h4>{age}</h4>
+      <h4>{message}</h4>
+      <button onClick={() => changeUseMessage("new message")} className="btn">
         change message
       </button>
     </>
