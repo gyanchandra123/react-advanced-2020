9646ba379f62692014e8fac030c4decce24ebbfa
gyanchandra moirangthem <gyanchandra moirangthem>
2021-07-04 23:21:19 +0530

    53. Multiple Returns - Fetching Example

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,15 +1,39 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 const url = "https://api.github.com/users/QuincyLarson";
 
 const MultipleReturns = () => {
+  const [isLoading, setisLoading] = useState(true);
+  const [isError, setisError] = useState(false);
+  const [user, setuser] = useState("default user");
 
-  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((user) => {
+        console.log(user);
+        const { login } = user.data;
+        setuser(login);
+        setisLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setisLoading(false);
+        setisError(true);
+      });
 
-  if (loading) {
+    return () => {};
+  }, []);
+
+  if (isLoading) {
     return <h3>Loading .....</h3>;
   }
 
-  return <h3>multiple returns</h3>;
+  if (isError) {
+    return <h3>ERROR .....</h3>;
+  }
+
+  return <h3>{user}</h3>;
 };
 
 export default MultipleReturns;
