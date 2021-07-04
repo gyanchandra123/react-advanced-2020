57193ecd88822d231312efedef2c42c9bb4a88d1
gyanchandra moirangthem <gyanchandra moirangthem>
2021-07-05 00:00:20 +0530

    54 . shorcircuit operation

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, settext] = useState("a");
 
-  return <h2>short circuit</h2>;
+  /* const firstValue = text || "hello world";
+  const secondValue = text && "hello world";
+
+  return (
+    <>
+      <h2>1st:{firstValue}</h2>
+      <h2>2nd :{secondValue}</h2>
+    </>
+  ); */
+
+  return (
+    <>
+      <h1>{text || "john doe"}</h1>
+      {text && <h2>hello world</h2>}
+    </>
+  );
 };
 
 export default ShortCircuit;
