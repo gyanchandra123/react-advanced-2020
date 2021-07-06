08c5a8d924e6545d92c522fe73645de67295c8ec
gyanchandra moirangthem <gyanchandra moirangthem>
2021-07-06 15:55:50 +0530

    79. React Router - Links

--- src/tutorial/11-react-router/setup/Navbar.js
+++ src/tutorial/11-react-router/setup/Navbar.js
@@ -1,7 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 const Navbar = () => {
-  return <nav>navbar</nav>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">home</Link>
+        </li>
+
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+
+        <li>
+          <Link to="/people">People</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
