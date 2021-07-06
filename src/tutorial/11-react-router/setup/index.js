512b5cd59d867ba1cb8ea7ec3dbeb1d6f50f742b
gyanchandra moirangthem <gyanchandra moirangthem>
2021-07-06 15:47:40 +0530

    78. React Router - Error and Switch Component

--- src/tutorial/11-react-router/setup/index.js
+++ src/tutorial/11-react-router/setup/index.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React from "react";
 // react router
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // pages
-import Home from './Home';
-import About from './About';
-import People from './People';
-import Error from './Error';
-import Person from './Person';
+import Home from "./Home";
+import About from "./About";
+import People from "./People";
+import Error from "./Error";
+import Person from "./Person";
 // navbar
-import Navbar from './Navbar';
+import Navbar from "./Navbar";
 const ReactRouterSetup = () => {
-  return <Router>
-      <Route exact path='/'>
-          <Home/>
-      </Route>
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-      <Route path='/about'>
-          <About/>
-      </Route>
+        <Route path="/about">
+          <About />
+        </Route>
 
-      <Route path='/people'>
-          <People/>
-      </Route>
-  </Router>;
+        <Route path="/people">
+          <People />
+        </Route>
+
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default ReactRouterSetup;
@@ -34,4 +42,4 @@ export default ReactRouterSetup;
 display:  '/' && 'people'
 2. solution : use exact
   - we can probably used the 'exact in every path of the components'
-*/ 
+*/
